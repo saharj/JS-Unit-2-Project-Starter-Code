853d025895cfc97c8f83f153e032cdb4c1cb7e8f
Sahar Jafari <shr.jafari@gmail.com>
2016-10-02 11:53:26 -0700

    Add Google Feed Api. Get the content from BBC.co.uk and change it to the JSON

--- js/app.js
+++ js/app.js
@@ -6,13 +6,52 @@
 'use strict';
 
 (function() {
+
+	google.load("feeds", "1");
+
+	function initialize() {
+    var feed = new google.feeds.Feed("http://www.bbc.co.uk/nature/wildlife/by/latest.rss");
+    
+    feed.load(function(result) {
+
+    	//var feed = result.feed;
+
+      if (!result.error) {
+      	console.log(result.feed);
+	    	var source = $('#entries-template').html();
+
+				var template = Handlebars.compile(source);
+				
+				var compileTemplate = template(result.feed);
+
+				$('#main').append(compileTemplate);
+
+        // var container = $('#main');
+        // for (var i = 0; i < result.feed.entries.length; i++) {
+        // 	(function() {
+        // 		var index = i;
+	       //    var entryTitle = result.feed.entries[index].title;
+	       //    //console.log(entryTitle);
+        // 	})();
+        // }
+      }
+    });
+  }
+  google.setOnLoadCallback(initialize);
+
+
 	$('.articleContent a').on('click', function() {
-		$('#popUp').removeClass('hidden');
+		$('#popUp').removeClass('hidden loader'); // TODO: only .hidden should be removed
 	});
 
 	$('.closePopUp').on('click', function() {
 		$('#popUp').addClass('hidden');
 	});
+
+	// TODO: Go back to the first page
+	// $('.container a').on('click', function() {
+
+	// });
 
 	$('#search a').on('click', function() {
 		if($('#search input').css('visibility') === 'visible') {
