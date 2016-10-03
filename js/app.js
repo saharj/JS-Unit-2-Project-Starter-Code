5bfbe8b35d6f46dbb960cf769233d1ef35fb0071
Sahar Jafari <shr.jafari@gmail.com>
2016-10-03 16:56:58 -0700

    Add snippets and show the title in popUp page

--- js/app.js
+++ js/app.js
@@ -6,8 +6,11 @@
 'use strict';
 
 (function() {
-
 	google.load("feeds", "1");
+
+	var takenFeed = {
+		entries: []
+	};
 
 	function initialize() {
     var feed = new google.feeds.Feed("http://www.bbc.co.uk/nature/wildlife/by/latest.rss");
@@ -15,35 +18,36 @@
     feed.setResultFormat(google.feeds.Feed.XML_FORMAT);
     feed.load(function(result) {
 
-    	var feed = {
-    		entries: []
-    	};
-
       if (!result.error) {
-      	console.log(result.xmlDocument);
-
       	var $xml = $(result.xmlDocument);
       	var items = $xml.find('item');
+      	
+      	console.log(result.xmlDocument);
 
       	items.each(function(i, item) {
       		var entry = {};
       		var date = $(item).find('pubDate').text();
       		var title = $(item).find('title').text();
+      		var content = $(item).find('description').text();
       		var img = $(item).find(':nth(3)').attr('url');
       		var d = new Date(date);
-      		var month = d.getMonth();
+      		//var month = d.getMonth() + 1;
+      		var year = d.getFullYear();
+      		console.log(year);
 
-      		entry.date = month + 1; // Still has works to do
+      		entry.contentSnippet = content.substr(0, 100) + '...';
+      		entry.date = year; // TODO: Let's add moths too
       		entry.title = title;
       		entry.image = img;
-      		feed.entries.push(entry);
+      		entry.descriptions = content;
+      		takenFeed.entries.push(entry); // push to entry to use the data for templating
       	})
 
 	    	var source = $('#entries-template').html();
 
 				var template = Handlebars.compile(source);
 				
-				var compileTemplate = template(feed);
+				var compileTemplate = template(takenFeed);
 
 				$('#main').append(compileTemplate);
       }
@@ -51,10 +55,22 @@
   }
   google.setOnLoadCallback(initialize);
 
+  console.log(takenFeed);
 
-	$('body').on('click', '.articleContent a' , function() {
-		console.log('clicked');
+	$('body').on('click', '.articleContent a' , function(event) {
+		console.log(event.target);
+		var ttl = $(event.target).text();
+		//var desc = feed
+		//var snippet = $(event.target).parent().siblings().text();
+
+		// $(descriptions).each(function(index, d) {
+		// 	if(d.substr(0, 100) === snippet) {
+		// 		$('.container p').text(d);
+		// 	}
+		// });
+
 		$('#popUp').removeClass('hidden loader'); // TODO: only .hidden should be removed (on load)
+		$('.container h1').text(ttl);
 	});
 
 	$('.closePopUp').on('click', function() {
